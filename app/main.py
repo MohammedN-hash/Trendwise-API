3029505c28033f7e27fe6a89483b37a2d644ed6e
mohammed <I443892>
2023-05-04 11:15:20 +0200

    fix server issue with uvicorn

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-
 import uvicorn
+from controller.getData import router as get_data
+
+
 
 
 app = FastAPI(
@@ -8,15 +10,19 @@ app = FastAPI(
     description="Anlys topic and get trends",
     version="1.0.0"
 )
-
-
 if __name__ == "__main__":
-  uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("UVICORN_PORT", 8080)), reload=True)
 
-  
+
 @app.get("/healthz")
 def get_request():
     return "Healthy"
+
+
+
+routes=[get_data]
+for route in routes:
+  app.include_router(route)
 
 
 
