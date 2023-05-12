24d99223f11700430a1a7a940d2484a3eefb46e3
mohammed <moodanwer@gmail.com>
2023-05-12 10:10:28 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -17,19 +17,19 @@ app.add_middleware(
     CORSMiddleware, allow_origins=['*'], allow_methods=allowed_methods, allow_headers=["*"])
 
 
+
 if __name__ == "__main__":
+    routes=[get_data]
+    for route in routes:
+        app.include_router(route)
+
+    @app.get("/healthz")
+    def get_request():
+        return "Healthy"
+
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("UVICORN_PORT", 8080)), reload=True)
 
 
-@app.get("/healthz")
-def get_request():
-    return "Healthy"
-
-
-
-routes=[get_data]
-for route in routes:
-  app.include_router(route)
 
 
 
