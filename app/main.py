a6885bc37f7c6233a2f9f075b0143cdad882e572
mohammed <moodanwer@gmail.com>
2023-05-12 10:15:41 +0200

    addd streamlit

--- app/main.py
+++ app/main.py
@@ -1,54 +1,27 @@
-from fastapi import FastAPI
-import uvicorn
-from controller.getData import router as get_data
-from starlette.middleware.cors import CORSMiddleware
-import os
-import subprocess
+import streamlit as st
+from requests import get
 
+# Define the URL for the FastAPI app
+API_URL = "http://localhost:8080"
 
-app = FastAPI(
-    title="TrendWise",
-    description="Anlys topic and get trends",
-    version="1.0.0"
-)
+# Define the Streamlit app
+def app():
 
-allowed_methods = ["POST", "PUT", "GET"]
+    # Add a title and description to the app
+    st.title("TrendWise")
+    st.write("Analyze topic and get trends")
 
-app.add_middleware(
-    CORSMiddleware, allow_origins=['*'], allow_methods=allowed_methods, allow_headers=["*"])
+    # Add a text input for the user to enter a query
+    query = st.text_input("Enter a query:")
 
+    # Add a button to submit the query
+    if st.button("Submit"):
+        # Call the FastAPI endpoint with the query as a parameter
+        response = get(f"{API_URL}/getAnalysis?query={query}").json()
 
+        # Display the response in Streamlit
+        st.write(response)
 
-
+# Run the Streamlit app
 if __name__ == "__main__":
-    routes=[get_data]
-    for route in routes:
-        app.include_router(route)
-
-    @app.get("/healthz")
-    def get_request():
-        return "Healthy"
-
-    # start uvicorn in a separate process
-    uvicorn_command = [
-        "uvicorn",
-        "main:app",
-        "--host", "0.0.0.0",
-        "--port", str(int(os.environ.get("UVICORN_PORT", 8080))),
-        "--reload",
-    ]
-    uvicorn_process = subprocess.Popen(uvicorn_command)
-
-    # start the Streamlit app
-    streamlit_command = [
-        "streamlit", "run", "my_app.py",
-        "--server.enableCORS", "false",
-        "--server.port", "8501",
-    ]
-    streamlit_process = subprocess.Popen(streamlit_command)
-
-    # wait for both processes to finish
-    uvicorn_process.wait()
-    streamlit_process.wait()
-
-
+    app()
