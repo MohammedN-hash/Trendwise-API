fa3e448313249e2acf233ef90da01c7c34f9cace
mohammed <moodanwer@gmail.com>
2023-05-20 10:59:53 +0200

    fix dates format

--- app/services/mangment.py
+++ app/services/mangment.py
@@ -1,6 +1,6 @@
 from services.reddit import get_reddits_with_comments
 from services.news import search_google_news,search_techcrunch,search_wired_articles
-from datetime import datetime
+import datetime
 
 def get_classfied_social_networks(query, fromDate, toDate, subreddit, post_limit, comment_limit): 
     try:
@@ -9,8 +9,8 @@ def get_classfied_social_networks(query, fromDate, toDate, subreddit, post_limit
         comment_limit = int(comment_limit)  
        
         # Format dates 
-        toDate = datetime.strptime(toDate, '%Y-%m-%d').strftime('%Y/%m/%d') if toDate != '' else None
-        fromDate = datetime.strptime(fromDate, '%Y-%m-%d').strftime('%Y/%m/%d') if fromDate != '' else None
+        toDate = datetime.strptime(toDate, '%Y-%m-%d') if toDate != '' else None
+        fromDate = datetime.strptime(fromDate, '%Y-%m-%d') if fromDate != '' else None
 
         reddit_posts_list, reddit_comments_list = get_reddits_with_comments(
             query, fromDate=fromDate, toDate=toDate, subreddit=subreddit, post_limit=post_limit, comment_limit=comment_limit
@@ -26,11 +26,9 @@ def get_classfied_news(query, fromDate, toDate, limit):
     try:
         # Convert limit to an integer
         limit = int(limit)  
-
-        toDate = datetime.strptime(toDate, '%Y/%m/%d').strftime('%Y/%m/%d') if toDate != '' else None
-        fromDate = datetime.strptime(fromDate, '%Y/%m/%d').strftime('%Y/%m/%d') if fromDate != '' else None
-
-
+        toDate = datetime.datetime.strptime(toDate, '%Y/%m/%d')  if toDate != '' else None
+        fromDate = datetime.datetime.strptime(fromDate, '%Y/%m/%d')  if fromDate != '' else None
+            
         google_news = search_google_news(query, fromDate, toDate, limit)
         techcrunch_news = search_techcrunch(query, fromDate, toDate, limit)
         wired_news = search_wired_articles(query, fromDate, toDate, limit)
--- app/services/news.py
+++ app/services/news.py
@@ -4,48 +4,58 @@ import json
 import re
 from bs4 import BeautifulSoup
 import feedparser
-import time
+
+from datetime import datetime
+
 
 from emotion_classfication_model.emotion_classfication_model import get_emotion
 
 
 def search_google_news(topic, from_date=None, to_date=None, limit=10):
-    # Set the API endpoint and parameters
-    endpoint = 'https://news.google.com/rss/search?q=' + \
-        topic + '&hl=en-US&gl=US&ceid=US:en'
-    limit = max(5, min(50, limit))  # Set the limit within the range of 5 to 50
+    try:
+        # Set the API endpoint and parameters
+        endpoint = 'https://news.google.com/rss/search?q=' + \
+            topic + '&hl=en-US&gl=US&ceid=US:en'
+        limit = max(5, min(50, limit))  # Set the limit within the range of 5 to 50
 
-    # Parse the RSS feed from the API
-    feed = feedparser.parse(endpoint)
+        # Parse the RSS feed from the API
+        feed = feedparser.parse(endpoint)
 
-    # Extract the articles from the feed
-    articles = []
-    for i, entry in enumerate(feed.entries):
+        # Extract the articles from the feed
+        articles = []
+        for i, entry in enumerate(feed.entries):
 
-        published_date = entry.published_parsed
-        if published_date >= from_date:
-            continue
-        if published_date <= to_date:
-            continue
+            published_date = entry.published_parsed
+            print(published_date)
+            published_date =datetime(*published_date[:6])
+            print(published_date)
+
+            if published_date >= from_date:
+                continue
+            if published_date <= to_date:
+                continue
 
 
-        if i == limit:
-            break
-        content = clean(entry.description)
-        if i == limit:
-            break
-        article = {
-            'title': entry.title,
-            'link': entry.link,
-            'content': content,
-            'published': entry.published,
-            'title_emotion': get_emotion(entry.title),
-            'content_emotion': get_emotion(content)
+            if i == limit:
+                break
+            content = clean(entry.description)
+            if i == limit:
+                break
+            article = {
+                'title': entry.title,
+                'link': entry.link,
+                'content': content,
+                'published': entry.published,
+                'title_emotion': get_emotion(entry.title),
+                'content_emotion': get_emotion(content)
 
-        }
-        articles.append(article)
+            }
+            articles.append(article)
 
-    return articles
+        return articles
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return articles
 
 
 def search_techcrunch(query, from_date=None, to_date=None, limit=10):
@@ -62,104 +72,125 @@ def search_techcrunch(query, from_date=None, to_date=None, limit=10):
         - link (str)
         - date (str)
     """
+    try:
+        API_ENDPOINT = "https://techcrunch.com/wp-json/wp/v2/posts"
+        limit = max(5, min(50, limit))  # Set the limit within the range of 5 to 50
 
-    API_ENDPOINT = "https://techcrunch.com/wp-json/wp/v2/posts"
-    limit = max(5, min(50, limit))  # Set the limit within the range of 5 to 50
+        query_params = {"search": query, "per_page": limit}
 
-    query_params = {"search": query, "per_page": limit}
+        response = requests.get(API_ENDPOINT, params=query_params)
+        artical_list = []
+        if response.ok:
+            articles = response.json()
+            if len(articles) >= 0:
 
-    response = requests.get(API_ENDPOINT, params=query_params)
-    artical_list = []
-    if response.ok:
-        articles = response.json()
-        if len(articles) == 0:
-            print("No articles found.")
+                for article in articles:
+
+                    published_date = article["date"]
+                    print(published_date)
+                    print(type(published_date))
+
+                    date_format = '%Y-%m-%dT%H:%M:%S'
+                    published_date = datetime.strptime(published_date, date_format)
+                    print(type(published_date))
+                    if published_date >= from_date:
+                        continue
+                    if published_date <= to_date:
+                        continue
+
+                    title = article["title"]["rendered"]
+                    link = article["link"]
+                    date = article["date"]
+                    content = clean(article['content']['rendered'][:500])
+                    title_emotion = get_emotion(title),
+                    content_emotion = get_emotion(content)
+
+                    artical_list.append({
+                        'title': title,
+                        'link': link,
+                        'content': content,
+                        'published': date,
+                        'title_emotion': title_emotion,
+                        'content_emotion': content_emotion
+                    })
+
+            else:
+                print("No articles found.")
+
         else:
+            print(f"Request failed with status code {response.status_code}")
+            print(response.content)
+
+        return artical_list
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return artical_list
+
+
+def search_wired_articles(topic, from_date=None, to_date=None, limit=10):
+    # Set the API endpoint and parameters
+    
+    try:
+        endpoint = 'https://www.wired.com/wp-json/wp/v2/posts'
+        limit = max(5, min(50, limit))  # Set the limit within the range of 5 to 50
+
+        params = {
+            'per_page': limit,     # Number of articles to fetch
+            'orderby': 'date',  # Order by publish date
+            'search': topic    # Search for articles on this topic
+        }
+
+        # Send the GET request to the API
+        response = requests.get(endpoint, params=params)
+
+        # Check if the request was successful (status code 200)
+        results = []
+        if response.status_code == 200:
+            # Parse the JSON response
+            articles = json.loads(response.content)
+
+            # Create a list to store the article titles and links
+
+            # Loop through the articles and add their titles and links to the list
             for article in articles:
 
-                published_date = article["date"]
-                published_date=time.strptime(published_date, '%Y-%m-%dT%H:%M:%S')
+                published_date=article["date"]
+                print(published_date)
+                print(type(published_date))
+
+                date_format = '%Y-%m-%dT%H:%M:%S'
+                published_date = datetime.strptime(published_date, date_format)
+                print(type(published_date))
+
                 if published_date >= from_date:
                     continue
                 if published_date <= to_date:
                     continue
 
-                title = article["title"]["rendered"]
-                link = article["link"]
-                date = article["date"]
+                title = article['title']['rendered']
+                link = article['link']
                 content = clean(article['content']['rendered'][:500])
+                date = article["date"]
                 title_emotion = get_emotion(title),
                 content_emotion = get_emotion(content)
 
-                artical_list.append({
+                results.append({
                     'title': title,
                     'link': link,
                     'content': content,
                     'published': date,
                     'title_emotion': title_emotion,
                     'content_emotion': content_emotion
+
                 })
-    else:
-        print(f"Request failed with status code {response.status_code}")
-        print(response.content)
 
-    return artical_list
-
-
-def search_wired_articles(topic, from_date=None, to_date=None, limit=10):
-    # Set the API endpoint and parameters
-    endpoint = 'https://www.wired.com/wp-json/wp/v2/posts'
-    limit = max(5, min(50, limit))  # Set the limit within the range of 5 to 50
-
-    params = {
-        'per_page': limit,     # Number of articles to fetch
-        'orderby': 'date',  # Order by publish date
-        'search': topic    # Search for articles on this topic
-    }
-
-    # Send the GET request to the API
-    response = requests.get(endpoint, params=params)
-
-    # Check if the request was successful (status code 200)
-    if response.status_code == 200:
-        # Parse the JSON response
-        articles = json.loads(response.content)
-
-        # Create a list to store the article titles and links
-        results = []
-
-        # Loop through the articles and add their titles and links to the list
-        for article in articles:
-
-            published_date=article["date"]
-            published_date=time.strptime(published_date,'%Y-%m-%dT%H:%M:%S')
-            if published_date >= from_date:
-                continue
-            if published_date <= to_date:
-                continue
-
-            title = article['title']['rendered']
-            link = article['link']
-            content = clean(article['content']['rendered'][:500])
-            date = article["date"]
-            title_emotion = get_emotion(title),
-            content_emotion = get_emotion(content)
-
-            results.append({
-                'title': title,
-                'link': link,
-                'content': content,
-                'published': date,
-                'title_emotion': title_emotion,
-                'content_emotion': content_emotion
-
-            })
-
-        # Return the list of results
+            # Return the list of results
+            return results
+        else:
+            print('Error: Failed to fetch articles')
+    except Exception as e:
+        print(f"An error occurred: {e}")
         return results
-    else:
-        print('Error: Failed to fetch articles')
-
 
 def clean(content):
 
