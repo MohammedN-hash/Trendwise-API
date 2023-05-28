60f153b5a6f0b996f9f6d22773796e666d821249
Mohammed Al-eryani <72073878+MohammedN-hash@users.noreply.github.com>
2023-05-28 09:54:08 +0200

    Update main_controller.py

--- app/controller/main_controller.py
+++ app/controller/main_controller.py
@@ -16,7 +16,7 @@ router = APIRouter(
 
 
 
-@router.get('/social-networks')
+@router.get('/social_networks')
 async def get_social_networks(query, from_date, to_date, subreddit,post_limit,comment_limit):
 
     reddit_posts_list,reddit_comments_list=get_classfied_social_networks(query, from_date, to_date, subreddit,post_limit,comment_limit)
@@ -29,13 +29,13 @@ async def get_social_networks(query, from_date, to_date, subreddit,post_limit,co
     # return the results as JSON
     return results
 
-@router.get('/google-trend')
+@router.get('/google_trend')
 async def get_google_trend(query,region, resolution, from_date, to_date):
 
     return get_trends(query, region=region, resolution=resolution, from_date=from_date, to_date=to_date,language='en-US' )
 
 
-@router.get('/get-news')
+@router.get('/get_news')
 async def get_news(query, from_date, to_date,limit):
 
     # Get data
@@ -43,8 +43,8 @@ async def get_news(query, from_date, to_date,limit):
                                                  
     # convert the results to a JSON-serializable format using jsonable_encoder
     results = {
-        "googleNews": jsonable_encoder(googleNews),
-        "techcrunchNews": jsonable_encoder(techcrunchNews),
+        "google_news": jsonable_encoder(googleNews),
+        "techcrunch_news": jsonable_encoder(techcrunchNews),
         "wired_news": jsonable_encoder(wired_news)
     }
     
